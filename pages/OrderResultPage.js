85f2cff1bd886234b8a115e682f7d7df8063cbde
DaryaMDK <DaryaMDK@users.noreply.github.com>
2024-08-27 13:10:12 +0000

    deploy: 5c07d9363efe27fc80b2663c94cd6190aa27b268

--- pages/OrderResultPage.js
+++
@@ -1,8 +0,0 @@
-export class OrderResultPage {
-  constructor(page) {
-    this.page = page;
-    this.backHomeButton = page.locator('#back-to-products');
-    this.completeHeaderTitle = page.locator('.complete-header');
-    this.finishButton = page.locator('#finish');
-  }
-}
